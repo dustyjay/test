279d319b02b8cd6673598e24eaaab26e01cad11f
dustyjay <38464407+dustyjay@users.noreply.github.com>
2018-04-17 21:26:09 +0100

    Add files via upload

---
+++ hng/hng_stage1.php
@@ -0,0 +1,34 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>
+			HNG4 | Stage1
+		</title>
+
+		<link rel="stylesheet" type="text/css" href="css/stage1.css">
+	</head>
+
+	<body>
+	
+		<div id="whole-page">
+			<div id="bg">
+				<img src="images/hng1.jpg">
+			</div>
+			
+			<div id="title">
+				<span>
+					HNG 4 INTERNSHIP 2018
+				</span>
+			</div>
+
+			<div id="desc">
+				<div style="width:100%">
+					<?php echo date("d-m-Y"); ?>
+				</div>
+				<div>
+					<?php echo date("H:i");	?>
+				</div>
+			</div>
+		</div>
+	</body>
+</html>
